66932db6707fe8c806dafe219ff43943d1031173
Roshan Bhardwaj <54205270+Roshanb79@users.noreply.github.com>
2021-04-06 14:40:20 +0530

    Update HelloServlet.java

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -11,17 +11,51 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MyServlet", 
-        urlPatterns = {"/hello"}
+        urlPatterns = {"/sv1"}
     )
 public class HelloServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
-        out.flush();
-        out.close();
+        String name=request.getParameter("name");
+       String services[]=request.getParameter("name");
+       StringBuffer serv=new StringBuffer();
+        HttpsSession ses=request.getSession(true);
+       int amount=0;
+      for(int i=0;i<services.length;i++){
+        serv.append(services[i]);
+       switch(services[i]){
+        case "Color":
+        amount+=250;
+        break;
+        case "Hair-Cut":
+        amount+=300;
+        break;
+        case "Head Massage":
+        amount+=450;
+        break;
+        case "Hair-Painting":
+        amount+=550;
+        break;
+    }
+}
+ses.putValue("name",name);
+ses.putValue("amount",name);
+ses.putValue("services",serv.toString());
+out.println("<html>");
+out.println("<body>");
+out.println("<form action='sv2.java' method='GET'>");
+out.println("Select Your Discount Coupon");
+out.println("<select name='disc' >
+out.println("<option value='10'>10%</option>");
+out.println("<option value='20'>20%</option>");
+out.println("<option value='30'>30%</option>");
+out.println("</select>");
+out.println("<input type='submit' value='proceed'>");
+out.println("<form>");
+out.println("</body>");
+out.println("</html>");
     }
     
 }
